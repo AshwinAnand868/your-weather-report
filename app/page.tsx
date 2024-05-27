72fd483edfd2f27b8e456128f47e15c27b2b77d9
AshwinAnand868 <anandashwin868@gmail.com>
2024-05-27 15:10:26 -0300

    Display City select box when country is selected with fetched data

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#394568] to-[#1837BE] p-10 flex flex-col justify-center items-center">
-      <Card className="max-w-4xl mx-auto">
+    <div className="min-h-screen bg-gradient-to-br from-[#394568] to-[#1837BE] p-10 flex flex-col items-center">
+      <Card className="max-w-4xl mx-auto mt-7">
         <Text className="text-4xl md:text-6xl font-bold text-center mb-10">Weather AI</Text>
         <Subtitle className="text-xl text-center">
           Powered by Open AI, Next.js 13.3, Tailwind CSS, Tremor 2.0 and more!
--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,44 +1,80 @@
 "use client";
 
-import { Country } from "country-state-city";
+import { GlobeAltIcon } from "@heroicons/react/16/solid";
+import { City, Country } from "country-state-city";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import Select from "react-select";
 
 const countryOptions = Country.getAllCountries().map((country) => {
-    return {
-        value: {
-            latitude: country.latitude,
-            longitude: country.longitude,
-            isoCode: country.isoCode
-        },
-        label: country.name,
-    }
+  return {
+    value: {
+      latitude: country.latitude,
+      longitude: country.longitude,
+      isoCode: country.isoCode,
+    },
+    label: country.name,
+  };
 });
 
-
 const CityPicker = () => {
+  const [selectedCountry, setSelectedCountry] = useState<CountryOption>(null);
+  const [selectedCity, setSelectedCity] = useState<CityOption>(null);
+  const router = useRouter(); //
 
-    const [selectedCountry, setSelectedCountry] = useState<CountryOption>(null);
-    const [selectedCity, setSelectedCity] = useState<CityOption>(null);
-    const router = useRouter(); // 
+  const handleSelectedCountry = (option: CountryOption) => {
+    setSelectedCountry(option);
+    // make the city null when user changes the country
+    setSelectedCity(null);
+  };
 
-    const handleSelectedCountry = (option: CountryOption) => {
-        setSelectedCountry(option);
-        // make the city null when user changes the country
-        setSelectedCity(null);
-    }
+  const handleSelectedCity = (cityOption: CityOption) => {
+    setSelectedCity(cityOption);
+    // router.push(`/location/${cityOption?.value.latitude}/${cityOption?.value.longitude}`);
+  }
 
   return (
-    <div>
+    <div className="space-y-4">
+      <div className="space-y-2">
+        <div className="flex space-x-2 text-white/80 items-center">
+          <GlobeAltIcon className="h-5 w-5 text-white" />
+          <label htmlFor="country">Country</label>
+        </div>
         <Select
-            className="text-black"
-            value={selectedCountry}
-            onChange={handleSelectedCountry}
-            options={countryOptions}
+          className="text-black"
+          value={selectedCountry}
+          onChange={handleSelectedCountry}
+          options={countryOptions}
         />
-    </div>
-  )
-}
+      </div>
 
-export default CityPicker
+      {selectedCountry && (
+        <div className="space-y-2">
+          <div className="flex space-x-2 text-white/80 items-center">
+            <GlobeAltIcon className="h-5 w-5 text-white" />
+            <label htmlFor="city">City</label>
+          </div>
+          <Select
+            className="text-black"
+            value={selectedCity}
+            onChange={handleSelectedCity}
+            options={City.getCitiesOfCountry(selectedCountry.value.isoCode)?.map(city => (
+                {
+                    value: {
+                        latitude: city.latitude!,
+                        longitude: city.longitude!,
+                        countryCode: city.countryCode,
+                        name: city.name,
+                        stateCode: city.stateCode
+                    },
+                    label: city.name
+                }
+            ))}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CityPicker;
