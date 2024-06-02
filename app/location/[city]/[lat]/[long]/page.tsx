096f2987acd7e1cb6a76c0d2f6878d77b3b7e817
AshwinAnand868 <anandashwin868@gmail.com>
2024-06-01 22:48:49 -0300

    Add and test fetch weather query

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,18 +1,36 @@
+import { getClient } from "@/apollo-client";
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+
 interface Props {
-    params : {
-        city: string,
-        lat: string,
-        long: string
-    }
+  params: {
+    city: string;
+    lat: string;
+    long: string;
+  };
 }
 
-const WeatherPage = ({params}: Props) => {
+const WeatherPage = async ({ params }: Props) => {
+  const { city, lat, long } = params;
+  const client = getClient();
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: "true",
+      longitude: long,
+      latitude: lat,
+      timezone: 'GMT'
+    }
+  });
 
-    const {city, lat, long} = params;
+  const result: Root = data.myQuery;
+
+  console.log(result);
 
   return (
-    <div>Welcome to weather page {city} {lat} {long}</div>
-  )
-}
+    <div>
+      Welcome to weather page {city} {lat} {long}
+    </div>
+  );
+};
 
-export default WeatherPage
+export default WeatherPage;
---
+++ graphql/queries/fetchWeatherQueries.ts
@@ -0,0 +1,92 @@
+import { gql } from "@apollo/client";
+
+const fetchWeatherQuery = gql`
+  query MyQuery(
+    $current_weather: String
+    $daily: String = "weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max"
+    $hourly: String = "temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,wind_gusts_10m,uv_index,uv_index_clear_sky"
+    $latitude: String!
+    $longitude: String!
+    $timezone:String!
+  ) {
+    myQuery(
+      current_weather: $current_weather
+      daily: $daily
+      hourly: $hourly
+      latitude: $latitude
+      longitude: $longitude
+      timezone: $timezone
+    ) {
+      latitude
+      longitude
+      timezone
+      timezone_abbreviation
+      utc_offset_seconds
+      generationtime_ms
+      current_weather {
+        is_day
+        temperature
+        time
+        weathercode
+        winddirection
+        windspeed
+      }
+      daily {
+        apparent_temperature_max
+        apparent_temperature_min
+        sunrise
+        sunset
+        temperature_2m_max
+        temperature_2m_min
+        time
+        weather_code
+        uv_index_max
+        uv_index_clear_sky_max
+      }
+      daily_units {
+        apparent_temperature_max
+        apparent_temperature_min
+        sunrise
+        sunset
+        temperature_2m_min
+        temperature_2m_max
+        time
+        uv_index_clear_sky_max
+        uv_index_max
+        weather_code
+      }
+      hourly {
+        apparent_temperature
+        precipitation
+        precipitation_probability
+        rain
+        relative_humidity_2m
+        showers
+        snow_depth
+        snowfall
+        temperature_2m
+        time
+        uv_index
+        uv_index_clear_sky
+        wind_gusts_10m
+      }
+      hourly_units {
+        apparent_temperature
+        precipitation
+        precipitation_probability
+        rain
+        relative_humidity_2m
+        snow_depth
+        showers
+        snowfall
+        temperature_2m
+        time
+        uv_index
+        uv_index_clear_sky
+        wind_gusts_10m
+      }
+    }
+  }
+`;
+
+export default fetchWeatherQuery;
