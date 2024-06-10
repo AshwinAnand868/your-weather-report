8b231ab592ec156fd9dc89040341734175c56f8c
AshwinAnand868 <anandashwin868@gmail.com>
2024-06-10 00:23:11 -0300

    Add function for cleaning data to be sent to api

---
+++ lib/cleanData.ts
@@ -0,0 +1,43 @@
+const cleanData = (data: Root, city: string) => {
+  const {
+    current_weather,
+    timezone,
+    hourly,
+    hourly_units,
+    timezone_abbreviation,
+  } = data;
+
+  const { temperature, windspeed, winddirection, weathercode, time } =
+    current_weather;
+
+  const {
+    temperature_2m,
+    snowfall,
+    rain,
+    relative_humidity_2m,
+    precipitation_probability,
+    uv_index,
+  } = hourly;
+
+  return {
+    current_weather: {
+      temperature,
+      winddirection,
+      windspeed,
+      time,
+      weathercode,
+    },
+    hourly: {
+      temperature_2m: temperature_2m.slice(0, 24),
+      snowfall: snowfall.slice(0, 24),
+      rain: rain.slice(0, 24),
+      relative_humidity_2m: relative_humidity_2m.slice(0, 24),
+      precipitation_probability: precipitation_probability.slice(0, 24),
+      uv_index: uv_index.slice(0, 24),
+    },
+    timezone,
+    timezone_abbreviation,
+    hourly_units,
+    city,
+  };
+};
