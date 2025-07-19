a858941f8e1ae2ffaf32f8c25e9b3261420dc3e8
AshwinAnand868 <anandashwin868@gmail.com>
2025-07-19 18:18:59 +0530

    Update LLM model of Groq to fix backend issue

--- app/api/getWeatherSummary/route.ts
+++ app/api/getWeatherSummary/route.ts
@@ -20,7 +20,7 @@ export async function POST(request: Request) {
         )}`,
       },
     ],
-    model: "mixtral-8x7b-32768",
+    model: "llama-3.3-70b-versatile",
     temperature: 0.8,
     stream: false,
     n:1,
